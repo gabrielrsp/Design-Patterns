67fa93a71ab72769928b050a5b2e460a079f4394
gabrielrsp <gabrielrsp18@gmail.com>
2019-12-19 13:25:11 -0300

    implement factory design pattern

---
+++ src/creationalPatterns/factory/BigEnemyShip.java
@@ -0,0 +1,13 @@
+
+package creationalPatterns.factory;
+
+public class BigEnemyShip extends EnemyShip {
+
+    public  BigEnemyShip(){
+
+        setName("Big Enemy Ship");
+        setDamage(40.00);
+
+    }
+
+}
---
+++ src/creationalPatterns/factory/RocketEnemyShip.java
@@ -0,0 +1,12 @@
+package creationalPatterns.factory;
+
+public class RocketEnemyShip extends EnemyShip {
+
+    public  RocketEnemyShip(){
+
+        setName("Rocket Enemy Ship");
+        setDamage(10.00);
+
+    }
+
+}
