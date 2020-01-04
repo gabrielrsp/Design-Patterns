91e8419a26c39adbd2638d1686bca1b6c4cc3db1
gabrielrsp <gabrielrsp18@gmail.com>
2020-01-04 13:30:23 -0300

    implement memento pattern

---
+++ src/behavioralPatterns/memento/Caretaker.java
@@ -0,0 +1,13 @@
+package behavioralPatterns.memento;
+
+import java.util.ArrayList;
+
+public class Caretaker {
+
+    ArrayList<Memento> savedArticles = new ArrayList<Memento>();
+
+    public void addMemento(Memento m){ savedArticles.add(m); }
+
+    public Memento getMemento(int index){ return savedArticles.get(index); }
+
+}
---
+++ src/behavioralPatterns/memento/Memento.java
@@ -0,0 +1,12 @@
+package behavioralPatterns.memento;
+
+public class Memento {
+
+    private String article;
+
+    public Memento(String articleSave){ article = articleSave; }
+
+    public String getSavedArticle(){ return article; }
+
+
+}
