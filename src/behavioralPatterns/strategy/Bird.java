fb09264fd19595cc6e76bf5fba6b8f2bae0fddd0
gabrielrsp <gabrielrsp18@gmail.com>
2020-01-06 21:20:13 -0300

    implement strategy pattern

---
+++ src/behavioralPatterns/strategy/Bird.java
@@ -0,0 +1,14 @@
+package behavioralPatterns.strategy;
+
+public class Bird extends Animal {
+
+    public Bird(){
+        super();
+
+        setSound("Tweet");
+
+        flyingType = new ItFlys();
+
+    }
+
+}
