e43a5f9d3af3b8b45f1d2fc6a05eed782ee490d7
gabriel <gabrielrsp18@gmail.com>
2020-01-07 22:22:08 -0300

    implement template method pattern

---
+++ src/behavioralPatterns/templateMethod/SandwichSculptor.java
@@ -0,0 +1,19 @@
+package behavioralPatterns.templateMethod;
+
+public class SandwichSculptor {
+
+    public static void main(String[] args){
+
+        Hoagie cust12Hoagie = new ItalianHoagie();
+
+        cust12Hoagie.makeSandwich();
+
+        System.out.println();
+
+        Hoagie cust13Hoagie = new VeggieHoagie();
+
+        cust13Hoagie.makeSandwich();
+
+    }
+
+}
